ac214814fbde76d3b6cb316e93e83b708576422d
DexterParris <91732338+DexterParris@users.noreply.github.com>
2023-04-13 14:19:39 +0100

    Added EzySlice
    The last implementation of mesh slicing didn't work very well for what I was trying to do, so I have decided to use EzySlice instead. I was hesitant at first to use it as there doesn't seem to be much documentation. but after finding a youtube video explaining how it can be implemented in VR, I now understand how to perform basic tasks such as mesh slicing, adding rigidbodies to sliced meshes and applying a material to the newly created faces.

---
+++ Assets/Scripts/Slice/SliceListener.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SliceListener : MonoBehaviour
+{
+    bool _canSlice;
+
+    public Slicer slicer;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (_canSlice)
+        {
+            _canSlice = false;
+            slicer.isTouched = true;
+        }
+  
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        _canSlice = true;
+    }
+}
